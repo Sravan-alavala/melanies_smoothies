3d88904442f6e93e5cac68905c641cfae4f462ec
Sravan-alavala <sravan.alavala1986@gmail.com>
2025-03-26 23:28:41 +0530

    Update streamlit_app.py
    Added SEARCH_ON variable to panda's

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,8 +41,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('frui
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df =my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list =st.multiselect('Choose upto 5 ingredients:'
                                  , my_dataframe
@@ -56,11 +56,15 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(each_fruit +' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
         st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
-    st.write(ingredients_string)  
+    #st.write(ingredients_string)  
 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
